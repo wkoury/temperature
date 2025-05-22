2b2d9813432565c5136af28e476cadcf87ff1250
Will Koury <Will Koury>
2025-05-22 00:07:40 -0500

    get container working

--- cmd/main.go
+++ cmd/main.go
@@ -1,27 +1,44 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"temperature/internal/db"
 	"temperature/internal/gpio"
 	"temperature/internal/temp"
 	"time"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	viper.AutomaticEnv()
+	db.Init()
+
 	deviceFile, err := gpio.FindDeviceFile()
 	if err != nil {
 		log.Fatalf("Error finding device: %v", err)
 	}
+
+	ctx := context.Background()
 
 	fmt.Printf("Reading temperature from %s\n", deviceFile)
 	for {
 		tempC, err := gpio.ReadTempC(deviceFile)
 		if err != nil {
 			log.Printf("Error reading temperature: %v", err)
-		} else {
-			fmt.Printf("Temperature: %.3f °F\n", temp.CtoF(tempC))
+			time.Sleep(10 * time.Minute)
+			continue
 		}
-		time.Sleep(1 * time.Second)
+
+		fmt.Printf("Temperature: %.3f °F\n", temp.CtoF(tempC))
+		err = db.InsertTemperatureRow(ctx, tempC, time.Now())
+		if err != nil {
+			log.Printf("Error inserting temperature row: %v", err)
+			time.Sleep(10 * time.Minute)
+			continue
+		}
+
+		time.Sleep(5 * time.Minute)
 	}
 }
